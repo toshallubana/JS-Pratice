acc8259ec7e55a1050c559e3c15babb3f8840933
toshallubana <toshal.t1@gmail.com>
2022-04-27 13:14:56 +0530

    flatten obj

--- BasicsQuestions/flattenObj.js
+++ BasicsQuestions/flattenObj.js
@@ -46,3 +46,33 @@ Object.entries(obj).reduce((r, [key, val]) => {
 }, res)
 
 flatten(obj)
+
+
+///////////////////////////////////////////////////
+const obj = {
+  name: "Toshal",
+  lastName: "Lubana",
+  address: {
+      pincode: 134107,
+      houseNumber: 469,
+      fullAddress: {
+          full: "chandimandir"
+      }
+  }
+}
+
+// Output
+// const obj = {"name": "Toshal","lastName":"Lubana","address.pincode": "134107","address.houseNumber": 469, "address.fullAddress.fullAddress":"chandimandir"}
+
+function objFlatten(obj,addingDot = '',output = {}){
+  for(let key in obj){
+      let k = `${addingDot}${key}`;
+      if(typeof obj[key] == "object"){
+          objFlatten(obj[key],`${k}.`,output);
+      }else{
+          output[k] = obj[key];
+      }
+  }
+  return output;
+}
+console.log(objFlatten(obj));
