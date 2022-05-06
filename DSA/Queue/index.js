95b4b6b8d95fb762f59efc308c26e4530260947d
toshallubana <toshal.t1@gmail.com>
2022-05-07 03:26:58 +0530

    implement queue

---
+++ DSA/Queue/index.js
@@ -0,0 +1,47 @@
+ // Queue
+ // complexity BIG O
+// Insertion O(1)
+// Removel O(1)
+// Searching O(N)
+// Access O(N)
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    cconstructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(value){
+        let newNode = new Node(value);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return this.size++;
+    }
+    dequeue(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(101);
+queue.enqueue(101);
+queue.enqueue(102);
