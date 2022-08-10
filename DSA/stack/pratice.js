6876dccd08c7c92ce85f3becf432cbd0fb17f6f4
toshallubana <toshal.t1@gmail.com>
2022-08-11 00:13:23 +0530

    implement stack using array

---
+++ DSA/stack/pratice.js
@@ -0,0 +1,54 @@
+// using array
+
+class Stack {
+
+    constructor(size) {
+        this.stack = [];
+        this.size = -1;
+    }
+
+    push(value) {
+        if(value) {
+            this.size++;
+            this.stack[this.size] = value;
+        }else{
+            console.log("stack overflow");
+        }
+    }
+
+    pop(){
+        if(this.size >= 0) {
+            this.stack.length = this.size--;
+        }
+        else{
+            console.log("stack underflow");
+        }
+    }
+
+    peek() {
+        if(this.size >= 0) {
+            return this.stack[this.size];
+        }else {
+            console.log("stack is empty");
+            return -1;
+        }
+    }
+
+    isEmpty() {
+        if(this.size == -1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
+
+let stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+stack.pop();
+console.log(stack.peek());
+console.log(stack.isEmpty());
+console.log(stack);
