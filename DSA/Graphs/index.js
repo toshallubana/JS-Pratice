69cfa3935e38c62228707131c529fd578f639ed8
toshallubana <toshal.t1@gmail.com>
2022-09-01 23:08:04 +0530

    graphs

--- DSA/Graphs/index.js
+++ DSA/Graphs/index.js
@@ -24,14 +24,91 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbour => {
+                if(!visited[neighbour]) {
+                    return dfs(neighbour);
+                }
+            });
+        })(start);
+        return result;
+    }
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+        while(stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighbour => {
+                if(!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    stack.push(neighbour);
+                }
+            });
+        }
+        return result;
+    }
+    breadthFirst(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        visited[start] = true;
+        let currentVertex;
+        while(queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighbour => {
+                if(!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    queue.push(neighbour);
+                }
+            })
+        }
+        return result;
+    }
 }
 const graph = new Graph();
-graph.addVertex('canada');
-graph.addVertex('india');
-graph.addVertex('america');
-graph.addEdge('india','canada');
-graph.addEdge('india','america');
-graph.removeEdge('india','america');
-graph.removeVertex('india');
-graph.adjacencyList['canada'].push("chandimandir");
+// graph.addVertex('canada');
+// graph.addVertex('india');
+// graph.addVertex('america');
+// graph.addEdge('india','canada');
+// graph.addEdge('india','america');
+// graph.removeEdge('india','america');
+// graph.removeVertex('india');
+// graph.adjacencyList['canada'].push("chandimandir");
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addEdge("A","B");
+graph.addEdge("A","C");
+graph.addEdge("B","D");
+graph.addEdge("C","E");
+graph.addEdge("D","E");
+graph.addEdge("D","F");
+graph.addEdge("E","F");
+// console.log(graph.depthFirstRecursive("A"));
+console.log(graph.depthFirstIterative("A"));
+console.log(graph.breadthFirst("A"));
 console.log(graph);
+
+    //   A
+    // /    \
+    // B     C
+    // |     | 
+    // D --- E 
+    //  \   /
+    //    F
