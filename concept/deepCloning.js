1e209d030587a879aec74bb8b58e6b6c1f324fd7
toshallubana <toshal.t1@gmail.com>
2022-12-08 01:21:46 +0530

    pratice

--- concept/deepCloning.js
+++ concept/deepCloning.js
@@ -1,53 +1,59 @@
 const obj = {
-    name: "Toshal",
-    lastName: "Lubana",
-    address: {
-        houseNumber: 469,
-        pinCode: 134107,
-        details:{
-            phoneNumber: 1234567890
+    "name": "download",
+    "actionConfig": {
+        "mimeType": "text/csv",
+        "type": "actionWithIcon",
+        "url": {
+            "refId": "actionDownloadClaims",
+            "params": {}
         }
+    },
+    "type": "callback",
+    "visibility": "downloadClaimsAction",
+    "ariaLabel": {
+        "refId": "displayTextTitleDownload",
+        "value": ""
     }
 }
 
 const obj2 = obj;
-console.log(obj2.address == obj.address);
-console.log(obj2.address.details == obj.address.details);
+// console.log(obj2.address == obj.address);
+// console.log(obj2.address.details == obj.address.details);
 
-// function deepClone(obj){
-//     const deepClonedobj = {};
+function deepClone(obj){
+    const deepClonedobj = {};
 
-//     for(let key in obj){
-//         if(typeof obj[key] == 'object' && typeof obj[key] !==  null){
-//             deepClonedobj[key] = deepClone(obj[key]);
-//         }else{
-//             deepClonedobj[key] = obj[key];
-//         }
-//     }
-//     return deepClonedobj;
-// }
+    for(let key in obj){
+        if(typeof obj[key] == 'object' && typeof obj[key] !==  null){
+            deepClonedobj[key] = deepClone(obj[key]);
+        }else{
+            deepClonedobj[key] = obj[key];
+        }
+    }
+    return deepClonedobj;
+}
 
-// const deep = deepClone(obj);
-// console.log(deep);
+const deep = deepClone(obj);
+console.log(deep);
 // console.log(obj2.address.details == deep.address.details);
 
 
 // Another approch
 
-const deepCloneMethod = (obj) => {
+// const deepCloneMethod = (obj) => {
 
-    if(typeof obj != "object" || obj === null) return obj;
+//     if(typeof obj != "object" || obj === null) return obj;
 
-    const newObject = Array.isArray(obj) ? [] : {};
+//     const newObject = Array.isArray(obj) ? [] : {};
     
-    for(let key in obj) {
-        const value = obj[key];
-        newObject[key] = deepCloneMethod(value);
-    }
+//     for(let key in obj) {
+//         const value = obj[key];
+//         newObject[key] = deepCloneMethod(value);
+//     }
 
-    return newObject;
-}
+//     return newObject;
+// }
 
-const deepy = deepCloneMethod(obj);
+// const deepy = deepCloneMethod(obj);
 // console.log(deepy);
-console.log(obj2.address.details == deepy.address.details);
+// console.log(obj2.address.details == deepy.address.details);
